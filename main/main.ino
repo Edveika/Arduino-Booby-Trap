812d11ce779c123669737037ec3510e4c453c21a
Edveika <edvinasbureika@gmail.com>
2024-02-25 20:39:10 +0200

    Bomb ignition logic

--- main/main.ino
+++ main/main.ino
@@ -1,33 +1,38 @@
 const int trigPin = 10;  
 const int echoPin = 9; 
 const int btnPin = 2;
+const float lethalDistance = 200.0;
 
 float duration, distance;  
+bool bExploded = false;
+
+void playUltraSound();
+void calcDistance();
 
 void setup() {  
   pinMode(trigPin, OUTPUT);  
   pinMode(echoPin, INPUT);  
   pinMode(btnPin, INPUT);
-  Serial.begin(9600);  
 }  
 
-void loop() {  
+void loop() {   
+  playUltraSound();
+  calcDistance();
+
+  if (distance <= lethalDistance && !bExploded)
+  {
+    bExploded = true;
+  }
+}
+
+void playUltraSound() {
   digitalWrite(trigPin, LOW);  
   delayMicroseconds(2);  
   digitalWrite(trigPin, HIGH);  
   delayMicroseconds(10);  
-  digitalWrite(trigPin, LOW);  
+  digitalWrite(trigPin, LOW); 
+}
+void calcDistance() {
   duration = pulseIn(echoPin, HIGH);  
   distance = (duration*.0343)/2;  
-  Serial.print("Distance: ");  
-  Serial.println(distance);  
-
-   int buttonState = LOW;
-   buttonState = digitalRead(btnPin);
-    if (buttonState == HIGH)
-    {
-        Serial.print("fu2 "); 
-    }
-  delay(100);  
-
 }
