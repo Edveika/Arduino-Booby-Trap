133f23462e55aad1458fbb24b15c2fb25e1fab7f
Edveika <edvinasbureika@gmail.com>
2024-04-09 00:03:59 +0300

    Removed duplicate code

--- main/main.ino
+++ main/main.ino
@@ -35,38 +35,18 @@ const int trigPin = 10;
 const int echoPin = 9; 
 const float lethalDistance = 500.0;
 
-float duration, distance;  
 bool bExploded = false;
 
-void playUltraSound();
-void calcDistance();
-
-void setup() {  
-  pinMode(ledPin, OUTPUT);  
-  pinMode(btnPin, INPUT);
+void setup()
+{  
   
-  digitalWrite(ledPin, LOW);
 }  
 
-void loop() { 
-  playUltraSound();
-  calcDistance();
- 
+void loop()
+{ 
   if (distance <= lethalDistance && distance > 0 && !bExploded)
   {
     bExploded = true;
     digitalWrite(ledPin, HIGH);
   }
-}
-
-void playUltraSound() {
-  digitalWrite(trigPin, LOW);  
-  delayMicroseconds(2);  
-  digitalWrite(trigPin, HIGH);  
-  delayMicroseconds(10);  
-  digitalWrite(trigPin, LOW); 
-}
-void calcDistance() {
-  duration = pulseIn(echoPin, HIGH);  
-  distance = (duration*.0343)/2;  
 }
